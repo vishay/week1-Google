27cd22084eb4d8f31ccf5d703b00b7d015215eed
Vishay Nihalani <vnihalani@lyft.com>
2016-01-31 14:19:05 -0800

    initial commit

---
+++ Google/SearchResultsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SearchResultsViewController.swift
+//  Google
+//
+//  Created by Vishay Nihalani on 1/30/16.
+//  Copyright © 2016 Vishay Nihalani. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTouchUpInsideBackButton(sender: AnyObject) {
+        
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
